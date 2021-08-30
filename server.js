5690f275c76d63af6e175d86fe158a3bbb530b65
Rao Adeel <adeel@localhost.localdomain>
2021-08-30 16:25:09 +0500

    set up routes in other saprate path

--- server.js
+++ server.js
@@ -2,26 +2,18 @@ const express = require('express');
 const dotenv = require("dotenv")
 const app = express();
 
+//routes Files
+const bootcamps = require('./routes/bootcamps')
+
+
 //Env Variables
 dotenv.config({ path: './config/config.env' });
 const PORT = process.env.PORT || 5000
 
-app.get('/api/v1/bootcamps', (req, res) => {
-    res.status(200).json({ success: true, msg: "Show all bootcamps" })
-})
-app.get('/api/v1/bootcamp', (req, res) => {
-    res.status(200).json({ scuccess: true, msg: "Show single bootcamp" })
-})
-app.post("/api/v1/bootcamp/:id", (req, res) => {
-    res.status(200).json({ success: true, msg: `Creat bootcamp ${req.params.id}` })
-})
-app.put('/api/v1/bootcamp/:id', (req, res) => {
-    res.status(200).json({ success: true, msg: `update bootcamp ${req.params.id}` })
-})
-app.delete('/api/v1/bootcamp/:id', (req, res) => {
-    res.status(200).json({ success: true, msg: `Delete Bootcamp ${req.params.id}` })
 
-})
+//mout the routes
+app.use('/api/v1/bootcamps', bootcamps)
+
 
 
 app.listen(PORT, console.log(`Server is listing at port ${PORT}`))
